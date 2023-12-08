b5ba561da20277aefe0fd237110c0eecbca7f511
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-12-08 13:03:18 +0200

    add slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,28 @@
 import Swiper from "swiper";
-import {Autoplay} from 'swiper/modules'
+import { Autoplay, Pagination, EffectFade } from 'swiper/modules'
 function swiperSlider() {
     const heroSlider = document.querySelectorAll('[data-slider="big-slider"]');
     if (heroSlider) {
         const slider = new Swiper('.swiper', {
-            modules: [Autoplay],
+            modules: [Pagination, EffectFade, Autoplay],
             // loop: true,
             slidesPerView: 1,
             spaceBetween: 0,
             centeredSlides: true,
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true,
+            },
             autoplay: {
-                delay: 2000,
-                disableOnInteraction: false,
+                delay: 5000,
+                disableOnInteraction: true,
                 waitForTransition: false,
             },
-            speed: 3000,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            speed: 1000,
         })
     }
 }
