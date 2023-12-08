6593e3f7bf82158554e37b041133cd6147703cf6
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-12-08 14:58:56 +0200

    add about page, refactor section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,11 @@
 import Swiper from "swiper";
-import { Autoplay, Pagination, EffectFade } from 'swiper/modules'
+import { Autoplay, Pagination, EffectFade, Navigation } from 'swiper/modules';
+
 function swiperSlider() {
-    const heroSlider = document.querySelectorAll('[data-slider="big-slider"]');
-    if (heroSlider) {
-        const slider = new Swiper('.swiper', {
+    const bigSlider = document.querySelector('[data-slider="big-slider"]');
+    const teamSlider = document.querySelector('[data-slider="team-slider"]');
+    if (bigSlider) {
+        const slider = new Swiper('[data-slider="big-slider"]', {
             modules: [Pagination, EffectFade, Autoplay],
             // loop: true,
             slidesPerView: 1,
@@ -23,7 +25,20 @@ function swiperSlider() {
                 clickable: true
             },
             speed: 1000,
-        })
+        });
+    }
+    if (teamSlider) {
+        const slider = new Swiper('[data-slider="team-slider"]', {
+            modules: [Navigation],
+            slidesPerView: 4,
+            spaceBetween: 20,
+            // centeredSlides: true,
+            speed: 1000,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            }
+        });
     }
 }
 window.addEventListener('load', swiperSlider);
